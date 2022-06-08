e1f677ed16a20a323f57daf1b29bcdb6aa8f14af
Torque (Tareq) El Dandachi <tareqdandachi@gmail.com>
2022-06-08 11:16:18 -0400

    added grid atomic implementation

--- src/.ipynb_checkpoints/QuantumCliffordCUDA-checkpoint.jl
+++ src/.ipynb_checkpoints/QuantumCliffordCUDA-checkpoint.jl
@@ -14,26 +14,88 @@ function test(l::CuArray) where T where L where M
     println(T)
 end
 
-function comm_CUDA(l::CuArray, r::CuArray, out::CuArray)
-    # ... complicated CUDA stuff
+# grid atomic implementation
+function comm_CUDA(l::AbstractArray, r::AbstractArray, out::AbstractArray)
+    
+    elements = blockDim().x*2
+    thread = threadIdx().x
+    block = blockIdx().x
+    offset = (block-1) * elements
+    
+    len = length(l) >> 1
+
+    # parallel reduction of values in a block
+    d = 1
+    while d < elements
+        sync_threads()
+        index = 2 * d * (thread-1) + 1
+        @inbounds if index <= elements && offset+index+d <= len
+            
+            # @cuprintln "block $block, thread $thread, $d, $offset = "
+            
+            if (d==1)
+                
+                if (offset+index+d == len+1)
+                    
+                    # @cuprintln "thread $thread, $d: $index, $(index+d) = "
+                    
+                    l[offset+index] = ((l[offset+index+len] & r[offset+index]) ⊻ (l[offset+index] & r[offset+index+len]))
+                    
+                else
+                    
+                    # @cuprintln "thread $thread, $d: $index, $(index+d) = (($(l[index+len]) & $(r[index])) ⊻ ($(l[index]) & $(r[index+len]))) ⊻ (($(l[index+len+d]) & $(r[index+d])) ⊻ ($(l[index+d]) & $(r[index+len+d]))) = $((l[index+len] & r[index]) ⊻ (l[index] & r[index+len])) ⊻ $((l[index+len+d] & r[index+d]) ⊻ (l[index+d] & r[index+len+d])) = $(((l[index+len] & r[index]) ⊻ (l[index] & r[index+len])) ⊻ ((l[index+len+d] & r[index+d]) ⊻ (l[index+d] & r[index+len+d])))"
+                
+                    l[offset+index] = ((l[offset+index+len] & r[offset+index]) ⊻ (l[offset+index] & r[offset+index+len])) ⊻
+                                      ((l[offset+index+len+d] & r[offset+index+d]) ⊻ (l[offset+index+d] & r[offset+index+len+d]))
+                    
+                end
+                
+            else
+                
+                if (offset+index+d >= len)
+                    
+                else
+            
+                    # @cuprintln "thread $thread, $d: $index, $(index+d) = $(l[index]) ⊻ $(l[index+d]) = $(l[index] ⊻ l[index+d])"
+
+                    l[offset+index] = l[offset+index] ⊻ l[offset+index+d]
+                    
+                end
+                
+            end
+            
+        end
+        
+        d *= 2
+    end
+    
+    # atomic reduction of this block's value
+    if thread == 1
+        # @cuprintln "$(out[]) XOR $(l[offset + 1])"
+        CUDA.@atomic out[] = out[] ⊻ l[offset + 1]
+        # CUDA.@atomic k[] = count_ones(k[])
+    end
+    
+    return
+    
 end
 
 @inline function QuantumClifford.comm(l::CuArray, r::CuArray)::UInt8
-    
-    println("QuantumClifford.comm thru QuantumCliffordCUDA")
 
     nthreads = CUDA.attribute(
         device(),
         CUDA.DEVICE_ATTRIBUTE_MAX_THREADS_PER_BLOCK
     )
     
-    nblocks = cld(length(l)/4, nthreads)
+    nblocks = cld(length(l), 4*nthreads)
     
-    out = CuArray([0])
+    out = CuArray([3])
     
     @cuda threads=nthreads blocks=nblocks comm_CUDA(l, r, out)
 
-    CUDA.@allowscalar out[1]
+    # convert(UInt8, count_ones(CUDA.@allowscalar out[1])) % 4
+    
+    count_ones(CUDA.@allowscalar out[1]) % 2
 
 end
 
--- test/.ipynb_checkpoints/test-checkpoint.jl
+++ test/.ipynb_checkpoints/test-checkpoint.jl
@@ -2,12 +2,23 @@ using CUDA
 using QuantumClifford
 using QuantumCliffordCUDA
 
-x = rand(UInt32, 1024*6*2*2)
-y = rand(UInt32, 1024*6*2*2)
+LARGE_COMPUTATION = 1024*1024*2*2
 
-cu_x = CuArray(x)
-cu_y = CuArray(y)
+for i = 1:10
 
-ans = QuantumClifford.comm(cu_x, cu_y)
+    x = rand(UInt32, 400)
+    y = rand(UInt32, 400)
 
-println(ans)
+    cu_x = CuArray(x)
+    cu_y = CuArray(y)
+
+    ans = QuantumClifford.comm(cu_x, cu_y)
+    ref = QuantumClifford.comm(x, y)
+
+    print("CUDA:")
+    println(ans)
+
+    print("REF: ")
+    println(ref)
+    
+end
