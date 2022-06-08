85a1f68404f515af6c8baa23305614c41195a8c6
Torque (Tareq) El Dandachi <tareqdandachi@gmail.com>
2022-06-08 11:31:39 -0400

    added tests - fails weird_Size

--- test/.ipynb_checkpoints/test-checkpoint.jl
+++ test/.ipynb_checkpoints/test-checkpoint.jl
@@ -2,23 +2,71 @@ using CUDA
 using QuantumClifford
 using QuantumCliffordCUDA
 
-LARGE_COMPUTATION = 1024*1024*2*2
+using Test
 
-for i = 1:10
+function small_test()
+    
+    println("small_test")
 
-    x = rand(UInt32, 400)
-    y = rand(UInt32, 400)
+    for i = 1:10
 
-    cu_x = CuArray(x)
-    cu_y = CuArray(y)
+        x = rand(UInt32, 400)
+        y = rand(UInt32, 400)
 
-    ans = QuantumClifford.comm(cu_x, cu_y)
-    ref = QuantumClifford.comm(x, y)
+        cu_x = CuArray(x)
+        cu_y = CuArray(y)
 
-    print("CUDA:")
-    println(ans)
+        ans = QuantumClifford.comm(cu_x, cu_y)
+        ref = QuantumClifford.comm(x, y)
 
-    print("REF: ")
-    println(ref)
+        Test.@test ans == ref
+
+    end
     
 end
+
+function large_test()
+    
+    println("large_test")
+
+    for i = 1:10
+
+        x = rand(UInt32, 1024*1024*2*2)
+        y = rand(UInt32, 1024*1024*2*2)
+
+        cu_x = CuArray(x)
+        cu_y = CuArray(y)
+
+        ans = QuantumClifford.comm(cu_x, cu_y)
+        ref = QuantumClifford.comm(x, y)
+
+        Test.@test ans == ref
+
+    end
+    
+end
+
+function weird_count()
+    
+    println("weird_count")
+
+    for i = 1:10
+
+        x = rand(UInt32, 1023*2)
+        y = rand(UInt32, 1023*2)
+
+        cu_x = CuArray(x)
+        cu_y = CuArray(y)
+
+        ans = QuantumClifford.comm(cu_x, cu_y)
+        ref = QuantumClifford.comm(x, y)
+
+        Test.@test ans == ref
+
+    end
+    
+end
+
+small_test()
+large_test()
+weird_count()
